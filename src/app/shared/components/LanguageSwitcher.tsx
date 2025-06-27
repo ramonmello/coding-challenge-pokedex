61fe779a8b35b813e81e0bad91b8a557e324d442
Ramon Mello <ramonomello@gmail.com>
2025-06-27 07:34:36 +0100

    feat: add LanguageSwitcher component for language selection

---
+++ src/app/shared/components/LanguageSwitcher.tsx
@@ -0,0 +1,21 @@
+import { SUPPORTED_LANGUAGES } from "@/main/config/i18n/config";
+import { Button } from "@/app/shared/components/ui/button";
+import { useTranslation } from "@/app/shared/hooks/useTranslation";
+
+export const LanguageSwitcher = () => {
+  const { i18n } = useTranslation("common");
+  return (
+    <>
+      {SUPPORTED_LANGUAGES.map((lang) => (
+        <Button
+          key={lang}
+          onClick={() => i18n.changeLanguage(lang)}
+          variant={i18n.language === lang ? "tonal" : "text"}
+          className="uppercase"
+        >
+          {lang}
+        </Button>
+      ))}
+    </>
+  );
+};
