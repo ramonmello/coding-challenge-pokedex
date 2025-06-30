3ffe43aef22d85c34f50aa4d91d701720a761910
Ramon Mello <ramonomello@gmail.com>
2025-06-30 07:43:55 +0100

    refactor: update PokemonList component for improved readability and consistency

--- src/app/features/pokemon/presentation/screens/PokemonList.tsx
+++ src/app/features/pokemon/presentation/screens/PokemonList.tsx
@@ -1,27 +1,27 @@
-import { LanguageSwitcher } from "@/app/shared/components";
-import { PokeCard, PokeSearch } from "../components";
-import { fakePokemonList } from "./tmp_fake_data";
+import { LanguageSwitcher } from '@/app/shared/components'
+import { PokeCard, PokeSearch } from '../components'
+import { fakePokemonList } from './tmp_fake_data'
 
 export const PokemonList = () => {
   return (
     <main>
-      <header className="sticky top-0 bg-white flex flex-col pt-4 px-6 md:flex-row md:items-center md:justify-between">
-        <div className="flex items-center justify-between md:justify-start md:gap-4">
-          <h1 className="h-11 leading-11 text-2xl font-bold">Pokédex</h1>
-          <div className="h-11 md:hidden flex gap-2">
+      <header className='sticky top-0 flex flex-col bg-white px-6 pt-4 md:flex-row md:items-center md:justify-between'>
+        <div className='flex items-center justify-between md:justify-start md:gap-4'>
+          <h1 className='h-11 text-2xl leading-11 font-bold'>Pokédex</h1>
+          <div className='flex h-11 gap-2 md:hidden'>
             <LanguageSwitcher />
           </div>
         </div>
-        <PokeSearch className="w-full md:w-auto md:order-none h-11 mt-4 md:mt-0" />
-        <div className="hidden h-11 md:block">
+        <PokeSearch className='mt-4 h-11 w-full md:order-none md:mt-0 md:w-auto' />
+        <div className='hidden h-11 md:block'>
           <LanguageSwitcher />
         </div>
       </header>
-      <section className="px-6 pt-4 flex gap-4 flex-col">
+      <section className='flex flex-col gap-4 px-6 pt-4'>
         {fakePokemonList.map((pokemon) => (
           <PokeCard key={pokemon.id} {...pokemon} />
         ))}
       </section>
     </main>
-  );
-};
+  )
+}
