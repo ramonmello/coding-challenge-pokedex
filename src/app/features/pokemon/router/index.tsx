0b284c4ff8d6f6a37e2856a99d164a281dca93bf
Ramon Mello <ramonomello@gmail.com>
2025-06-30 07:32:00 +0100

    refactor: remove prefetchQuery

--- src/app/features/pokemon/router/index.tsx
+++ src/app/features/pokemon/router/index.tsx
@@ -1,17 +1,11 @@
-import { createRoute } from "@tanstack/react-router";
-import { rootRoute } from "@/main/router/config/router-config";
-import { PokemonList } from "../presentation/screens";
-import { pokemonListQueryOptions } from "../queries/pokemon-list";
+import { createRoute } from '@tanstack/react-router'
+import { rootRoute } from '@/main/router/config/router-config'
+import { PokemonList } from '../presentation/screens'
 
 const PokemonListRoute = createRoute({
   getParentRoute: () => rootRoute,
-  loader: ({ context }) => {
-    context.queryClient.prefetchQuery(pokemonListQueryOptions());
-  },
-  path: "/pokemon",
-  component: PokemonList,
-  // TODO: Add a loading component
-  pendingComponent: () => <div>Loading...</div>,
-});
+  path: '/pokemon',
+  component: PokemonList
+})
 
-export const pokemonRoutes = [PokemonListRoute];
+export const pokemonRoutes = [PokemonListRoute]
