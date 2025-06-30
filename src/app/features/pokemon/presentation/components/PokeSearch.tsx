38c88e37a87e693f32b349045c40d71799a3445c
Ramon Mello <ramonomello@gmail.com>
2025-06-30 12:05:21 +0100

    feat: add search Pokémon localization and integrate translation in PokeSearch component

--- src/app/features/pokemon/presentation/components/PokeSearch.tsx
+++ src/app/features/pokemon/presentation/components/PokeSearch.tsx
@@ -1,42 +1,43 @@
-import * as React from "react";
-
-import { SearchIcon, SendIcon } from "@/app/shared/components/icons";
-import { Button } from "@/app/shared/components/ui/button";
-import { cn } from "@/app/shared/utils";
+import * as React from 'react'
+import { SearchIcon, SendIcon } from '@/app/shared/components/icons'
+import { Button } from '@/app/shared/components/ui/button'
+import { cn } from '@/app/shared/utils'
+import { useTranslation } from 'react-i18next'
 
 export function PokeSearch({
   className,
   ...props
-}: React.ComponentProps<"input">) {
+}: React.ComponentProps<'input'>) {
+  const { t } = useTranslation(['common'])
   return (
     <div
       className={cn(
-        "flex outline-1 -outline-offset-1 outline-blue-medium rounded-lg h-11 bg-blue-light focus-within:outline-2 focus-within:outline-blue-dark/70",
+        'outline-blue-medium bg-blue-light focus-within:outline-blue-dark/70 flex h-11 rounded-lg outline-1 -outline-offset-1 focus-within:outline-2',
         className
       )}
     >
       <SearchIcon
-        aria-hidden="true"
-        className="pointer-events-none shrink-0 ml-2 mr-4 size-6 self-center text-blue-medium"
+        aria-hidden='true'
+        className='text-blue-medium pointer-events-none mr-4 ml-2 size-6 shrink-0 self-center'
       />
       <input
-        id="search-pokemon"
-        name="search-pokemon"
-        placeholder="Search Pokémon"
-        className="w-full focus:outline-none text-sm text-blue-dark placeholder:text-blue-medium placeholder:text-sm"
+        id='search-pokemon'
+        name='search-pokemon'
+        placeholder={t('search-pokemon')}
+        className='text-blue-dark placeholder:text-blue-medium w-full text-sm placeholder:text-sm focus:outline-none'
         {...props}
       />
 
       <Button
-        variant="text"
-        size="icon"
-        className="mr-2"
+        variant='text'
+        size='icon'
+        className='mr-2'
         onClick={() => {
-          console.log("clicked");
+          console.log('clicked')
         }}
       >
-        <SendIcon className="text-blue-medium size-5" />
+        <SendIcon className='text-blue-medium size-5' />
       </Button>
     </div>
-  );
+  )
 }
