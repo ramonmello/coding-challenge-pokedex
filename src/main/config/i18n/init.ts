41b56bf37a0406925c935d425dec6d34db790378
Ramon Mello <ramonomello@gmail.com>
2025-07-02 03:46:19 +0100

    refactor(i18n): update initialization options and improve configuration structure

--- src/main/config/i18n/init.ts
+++ src/main/config/i18n/init.ts
@@ -1,21 +1,23 @@
-import i18next, { type InitOptions } from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+import i18next, { type InitOptions } from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
 
-import { LOAD_PATH, SUPPORTED_LANGUAGES } from "./config";
+import { LOAD_PATH, SUPPORTED_LANGUAGES } from './config'
 
 const options: InitOptions = {
   debug: import.meta.env.DEV,
-  fallbackLng: "en",
+  fallbackLng: 'en',
   supportedLngs: SUPPORTED_LANGUAGES,
+  ns: ['common', 'card', 'modal'],
+  defaultNS: 'common',
   backend: {
-    loadPath: LOAD_PATH,
-  },
-};
+    loadPath: LOAD_PATH
+  }
+}
 
 export default i18next
   .use(initReactI18next)
   .use(LanguageDetector)
   .use(Backend)
-  .init(options);
+  .init(options)
