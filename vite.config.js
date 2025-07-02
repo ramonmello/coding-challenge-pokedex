f3f84e2b8fd9a103c408b8d512e81e1ccdf8119f
Ramon Mello <ramonomello@gmail.com>
2025-07-02 03:43:37 +0100

    refactor: update Vite configuration to use @vitejs/plugin-react-swc and adjust package dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import Inspect from "vite-plugin-inspect";
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import Inspect from 'vite-plugin-inspect'
 
-import { resolve } from "node:path";
+import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [viteReact(), tailwindcss(), Inspect()],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom'
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
-});
+      '@': resolve(__dirname, './src')
+    }
+  }
+})
