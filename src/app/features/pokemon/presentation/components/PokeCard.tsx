9bbf0fb1fa70702397210afe463f656f8f9b47a0
Ramon Mello <ramonomello@gmail.com>
2025-07-02 10:46:41 +0100

    feat(pokemon): disable card expand on desktop devices

--- src/app/features/pokemon/presentation/components/PokeCard.tsx
+++ src/app/features/pokemon/presentation/components/PokeCard.tsx
@@ -13,6 +13,7 @@ import { PokeTypes } from './PokeTypes'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { pokemonDetailsQueryOptions } from '@/app/features/pokemon/queries'
 import { cn } from '@/app/shared/utils'
+import { useMediaQuery } from '@/app/shared/hooks'
 
 type PokeCardProps = {
   name: string
@@ -21,6 +22,7 @@ type PokeCardProps = {
 export const PokeCard = ({ name }: PokeCardProps) => {
   const { t } = useTranslation(['common', 'card'])
   const { data } = useSuspenseQuery(pokemonDetailsQueryOptions({ name }))
+  const isMobile = useMediaQuery('(max-width: 768px)')
 
   return (
     <Collapsible asChild>
@@ -61,17 +63,19 @@ export const PokeCard = ({ name }: PokeCardProps) => {
           </div>
         </div>
         <div className='mt-2 flex justify-between'>
-          <CollapsibleTrigger asChild>
-            <Button variant='text' className='flex-1/2'>
-              <span className='group-data-[state=open]:hidden'>
-                {t('card:expand')}
-              </span>
-              <span className='hidden group-data-[state=open]:inline'>
-                {t('card:collapse')}
-              </span>
-              <ChevronDownIcon className='size-5 transition-transform duration-300 group-data-[state=open]:-rotate-180' />
-            </Button>
-          </CollapsibleTrigger>
+          {isMobile && (
+            <CollapsibleTrigger asChild>
+              <Button variant='text' className='flex-1/2'>
+                <span className='group-data-[state=open]:hidden'>
+                  {t('card:expand')}
+                </span>
+                <span className='hidden group-data-[state=open]:inline'>
+                  {t('card:collapse')}
+                </span>
+                <ChevronDownIcon className='size-5 transition-transform duration-300 group-data-[state=open]:-rotate-180' />
+              </Button>
+            </CollapsibleTrigger>
+          )}
           <ResponsiveDialog
             trigger={
               <Button variant='text' className='flex-1/2'>
